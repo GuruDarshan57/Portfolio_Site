ff395218ed7d79b3d4167d0452273ef547599580
Gurudarshan57 <dev560111@gmail.com>
2024-12-30 13:53:53 +0530

    added floating dock

--- components/Links.tsx
+++ components/Links.tsx
@@ -4,30 +4,35 @@ import { FaSquareGithub } from "react-icons/fa6";
 import { FaSquareXTwitter } from "react-icons/fa6";
 import { FaLinkedin } from "react-icons/fa6";
 import { IoMail } from "react-icons/io5";
+import { FloatingDock } from "./ui/FloatingDock";
+
+const links = [
+  {
+    title: "Github",
+    icon: <FaSquareGithub />,
+    href: "https://github.com/GuruDarshan57",
+  },
+  {
+    title: "X",
+    icon: <FaSquareXTwitter />,
+    href: "https://x.com/GuruDarshan_573",
+  },
+  {
+    title: "LinkedIn",
+    icon: <FaLinkedin />,
+    href: "https://www.linkedin.com/in/gurudarshan57/",
+  },
+  {
+    title: "Mail",
+    icon: <IoMail />,
+    href: "guru573116@gmail.com",
+  },
+];
 
 const Links = () => {
   return (
-    <div className="border-2 bottom-5 bg-white dark:bg-black fixed flex gap-6 px-5 p-2.5 rounded-md">
-      <Link
-        href={"https://github.com/GuruDarshan57"}
-        target="_blank"
-        className=""
-      >
-        <FaSquareGithub className="size-7 cursor-pointer" />
-      </Link>
-      <Link href={"https://x.com/GuruDarshan_573"} target="_blank" className="">
-        <FaSquareXTwitter className="size-7 cursor-pointer" />
-      </Link>
-      <Link
-        href={"https://www.linkedin.com/in/gurudarshan57/"}
-        target="_blank"
-        className=""
-      >
-        <FaLinkedin className="size-7 cursor-pointer" />
-      </Link>
-      <Link href={"guru573116@gmail.com"} target="_blank" className="">
-        <IoMail className="size-7 cursor-pointer" />
-      </Link>
+    <div className="bottom-5 bg-white dark:bg-black fixed">
+      <FloatingDock items={links} />
     </div>
   );
 };
