dbfa02e46c44fe9bcbe7a0d2460b7188dddc2648
guru <dev560111@gmail.com>
2025-04-04 16:10:04 +0530

    fix extras lg md sm view

--- app/page.tsx
+++ app/page.tsx
@@ -11,13 +11,13 @@ import Utilities from "@/components/Utilities";
 export default function Home() {
   return (
     <div className="dark:bg-black dark:text-white flex flex-col items-center h-screen w-screen relative">
-      <Utilities/>
       <Navbar />
       <div className="flex justify-center w-full max-h-full overflow-y-scroll scroll-smooth hidescrollbar">
         <div className="flex flex-col items-center w-11/12 sm:w-3/4 lg:w-2/3 xl:w-1/2 h-full">
           <Hero />
           <Projects />
           <About />
+          <Utilities/>
         </div>
       </div>
       <Links />
--- components/Extras.tsx
+++ components/Extras.tsx
@@ -29,21 +29,21 @@ const Utilities = () => {
     }
   };
   return (
-    <>
-      <div className="absolute top-8 left-8 justify-center items-center gap-2 tracking-wider hidden sm:flex">
+    <div className="w-full flex justify-between">
+      <div className="flex xl:absolute xl:top-8 xl:left-8 justify-center items-center gap-2 tracking-wider">
         <div className="blink_dot size-2 rounded-full bg-red-500"></div>
         <div className="clock">00:00:00 AM</div>
       </div>
-      <div className="border-r-2 border-b-2 pr-2 dark:border-slate-600 absolute top-8 right-8 tracking-wider hidden sm:flex flex-col">
-        <div className="text-sm">Views</div>
-        <div className="relative -top-2 -left-1 text-7xl"> {views ? views : "00"}</div>
+      <div className="flex xl:flex-col xl:border-r-2 xl:border-b-2 pr-2 xl:dark:border-slate-600 xl:absolute xl:top-8 xl:right-8 tracking-wider">
+        <div className="hidden xl:block  xl:text-sm">Views</div>
+        <div className="relative xl:-top-2 xl:-left-1 xl:text-7xl"> {views ? views : "000"}</div>
       </div>
-      <div className="absolute bottom-8 left-8 hidden sm:flex gap-2 items-center">
+      <div className="flex xl:absolute xl:bottom-8 xl:left-8 gap-2 items-center">
         <div className="bg-red-600 size-4"></div>
         <div className="bg-black dark:bg-white size-4 rounded-full"></div>
         <div className="border-red-600 border-l-[1rem] border-y-[0.5rem] border-y-transparent"></div>
       </div>
-    </>
+    </div>
   );
 };
 
