e760ed9c046b4a9a74718b223aa2146918753134
guru <dev560111@gmail.com>
2025-04-08 08:20:11 +0530

    fix scrolling issue

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ import Utilities from "@/components/Extras";
 export default function Home() {
   return (
     <div className="dark:bg-black dark:text-white flex flex-col items-center h-screen w-screen relative">
-      <div className="w-11/12 sm:w-3/4 lg:w-2/3 xl:w-1/2 flex flex-col items-center max-h-full overflow-y-scroll scroll-smooth hidescrollbar">
+      <div className="w-full px-2 sm:px-10 lg:px-20 xl:px-96 flex flex-col items-center max-h-full overflow-y-scroll scroll-smooth hidescrollbar">
         <Navbar />
         <Hero />
         <Projects />
